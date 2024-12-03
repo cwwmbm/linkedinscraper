3be1f12bdfa8f8811a40a2c850319187ec3cb1a1
david-bfg <david.1.bfg@gmail.com>
2024-12-03 13:14:10 -0600

    Refactor: replace SQLite with ORM in scraping file A full refactor of the main file that scrapes data. SQLite is out and SQLAlchemy is in. Improved code simplicity and removing around 90 LOC. DB model has also been extended to implement filtered jobs and include some additional default values.

--- models.py
+++ models.py
@@ -10,12 +10,12 @@ class Job(db.Model):
     date = db.Column(db.DateTime, nullable=True)
     job_url = db.Column(db.Text, nullable=True)
     job_description = db.Column(db.Text, nullable=True)
-    applied = db.Column(db.Integer, nullable=True)
-    hidden = db.Column(db.Integer, nullable=True)
-    titleRanking = db.Column(db.Float, nullable=True)
-    liked = db.Column(db.Integer, nullable=True)
-    interview = db.Column(db.Integer, nullable=True)
-    rejected = db.Column(db.Integer, nullable=True)
+    applied = db.Column(db.Integer, nullable=True, default=0)
+    hidden = db.Column(db.Integer, nullable=True, default=0)
+    titleRanking = db.Column(db.Float, nullable=True, default=None)
+    liked = db.Column(db.Integer, nullable=True, default=None)
+    interview = db.Column(db.Integer, nullable=True, default=0)
+    rejected = db.Column(db.Integer, nullable=True, default=0)
     date_loaded = db.Column(db.Text, nullable=True)
     cover_letter = db.Column(db.Text, nullable=True, default=None)
     resume = db.Column(db.Text, nullable=True, default=None)
@@ -24,6 +24,24 @@ class Job(db.Model):
         return (
             f"<Job(id={self.id}, title='{self.title}', company='{self.company}', "
             f"location='{self.location}', date='{self.date}')>"
+        )
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+class FilteredJob(db.Model):
+    __tablename__ = config['filtered_jobs_tablename']
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    company = db.Column(db.Text, nullable=True)
+    date = db.Column(db.DateTime, nullable=True)
+    job_url = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return (
+            f"<Job(id={self.id}, title='{self.title}', company='{self.company}', "
+            f"date='{self.date}')>"
         )
 
     def as_dict(self):
